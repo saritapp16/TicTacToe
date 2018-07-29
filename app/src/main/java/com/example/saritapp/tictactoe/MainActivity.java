019fcfe23530efc39d5b2a19cec58a63ad508077
saritapp16 <saritapp16@hotmail.com>
2018-07-28 21:28:19 -0400

    Game almost working

--- app/src/main/java/com/example/saritapp/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/saritapp/tictactoe/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.saritapp.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void startGame(View view){
+        Intent startNewGame = new Intent(this, GameBoard.class);
+        this.startActivity(startNewGame);
+    }
+
 }
