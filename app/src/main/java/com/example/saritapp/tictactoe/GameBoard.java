a5f53a2f2f4619f33ecb4fe01f550997715561ab
saritapp16 <saritapp16@hotmail.com>
2018-07-29 00:01:43 -0400

    [STYLE]Styling

--- app/src/main/java/com/example/saritapp/tictactoe/GameBoard.java
+++ app/src/main/java/com/example/saritapp/tictactoe/GameBoard.java
@@ -1,5 +1,6 @@
 package com.example.saritapp.tictactoe;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,6 +22,9 @@ public class GameBoard extends AppCompatActivity implements View.OnClickListener
     private TextView textViewPlayer1;
 
     private TextView textViewPlayer2;
+    int red = Color.parseColor("#FF5733");
+    int blue = Color.parseColor("#33B8FF");
+    int green = Color.parseColor("#15A810");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +47,16 @@ public class GameBoard extends AppCompatActivity implements View.OnClickListener
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                resetGame();
             }
         });
+    }
+
+    private void resetGame(){
+        player1Points = 0;
+        player2Points = 0;
+        updatePointsText();
+        resetBoard();
     }
 
     @Override
@@ -54,10 +65,14 @@ public class GameBoard extends AppCompatActivity implements View.OnClickListener
             return;
         }
 
+        Button pressed_button = (Button) v;
         if (player1Turn) {
-            ((Button) v).setText("X");
+
+            pressed_button.setTextColor(red);
+            pressed_button.setText("X");
         } else {
-            ((Button) v).setText("O");
+            pressed_button.setTextColor(blue);
+            pressed_button.setText("O");
         }
 
         roundCount++;
@@ -77,38 +92,58 @@ public class GameBoard extends AppCompatActivity implements View.OnClickListener
 
     private boolean checkForWin() {
         String[][] field = new String[3][3];
+        Button[][] _buttons = new Button[3][3];
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 field[i][j] = buttons[i][j].getText().toString();
+                _buttons[i][j] = buttons[i][j];
             }
         }
 
+        //Chequear si se ganó horizontalmente
         for (int i = 0; i < 3; i++) {
             if (field[i][0].equals(field[i][1])
                     && field[i][0].equals(field[i][2])
                     && !field[i][0].equals("")) {
+
+                _buttons[i][0].setTextColor(green);
+                _buttons[i][1].setTextColor(green);
+                _buttons[i][2].setTextColor(green);
                 return true;
             }
         }
 
+        //Chequear si se ganó verticalmente
         for (int i = 0; i < 3; i++) {
             if (field[0][i].equals(field[1][i])
                     && field[0][i].equals(field[2][i])
                     && !field[0][i].equals("")) {
+
+                _buttons[0][i].setTextColor(green);
+                _buttons[1][i].setTextColor(green);
+                _buttons[2][i].setTextColor(green);
                 return true;
             }
         }
 
+        //Chequear si se ganó diagonalmente a la izquierda
         if (field[0][0].equals(field[1][1])
                 && field[0][0].equals(field[2][2])
                 && !field[0][0].equals("")) {
+            _buttons[0][0].setTextColor(green);
+            _buttons[1][1].setTextColor(green);
+            _buttons[2][2].setTextColor(green);
             return true;
         }
 
+        //Chequear si se ganó diagonalmente a la derecha
         if (field[0][2].equals(field[1][1])
                 && field[0][2].equals(field[2][0])
                 && !field[0][2].equals("")) {
+            _buttons[0][2].setTextColor(green);
+            _buttons[1][1].setTextColor(green);
+            _buttons[2][0].setTextColor(green);
             return true;
         }
 
@@ -117,26 +152,26 @@ public class GameBoard extends AppCompatActivity implements View.OnClickListener
 
     private void player1Wins() {
         player1Points++;
-        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "¡Ganó el jugador 1!", Toast.LENGTH_LONG).show();
         updatePointsText();
         resetBoard();
     }
 
     private void player2Wins() {
         player2Points++;
-        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "¡Ganó el jugador 2!", Toast.LENGTH_LONG).show();
         updatePointsText();
         resetBoard();
     }
 
     private void draw() {
-        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Draw!", Toast.LENGTH_LONG).show();
         resetBoard();
     }
 
     private void updatePointsText() {
-        textViewPlayer1.setText("Player 1: " + player1Points);
-        textViewPlayer2.setText("Player 2: " + player2Points);
+        textViewPlayer1.setText("Jugador 1: " + player1Points);
+        textViewPlayer2.setText("Jugador 2: " + player2Points);
     }
 
     private void resetBoard() {
@@ -148,5 +183,25 @@ public class GameBoard extends AppCompatActivity implements View.OnClickListener
 
         roundCount = 0;
         player1Turn = true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player2Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
     }
 }
